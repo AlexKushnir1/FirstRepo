e6ddd0c13c39f60c3cddd1cb5e61a1f5f7ee1d69
AlexKushnir1 <alex@alex-Precision-7510>
2023-10-13 11:40:01 +0300

    added gitignore, connect server on localhost:8080 with reguest /

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,57 +1,38 @@
 package org.example;
 
-import java.util.Scanner;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
-        long binary1, binary2, multiply = 0;
-        int digit, factor = 1;
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input the first binary number: ");
-        binary1 = in.nextLong();
-        System.out.print("Input the second binary number: ");
-        binary2 = in.nextLong();
-        while (binary2 != 0)
-        {
-            digit = (int)(binary2 % 10);
-            if (digit == 1)
-            {
-                binary1 = binary1 * factor;
-                multiply = binaryproduct((int) binary1, (int) multiply);
-            }
-            else
-            {
-                binary1 = binary1 * factor;
-            }
-            binary2 = binary2 / 10;
-            factor = 10;
-        }
-        System.out.print("Product of two binary numbers: " + multiply+"\n");
-    }
-    static int binaryproduct(int binary1, int binary2)
-    {
-        int i = 0, remainder = 0;
-        int[] sum = new int[20];
-        int binary_prod_result = 0;
+    public static void main(String[] args) throws IOException {
+        int port = 8080; // Port to listen on
 
-        while (binary1 != 0 || binary2 != 0)
-        {
-            sum[i++] = (binary1 % 10 + binary2 % 10 + remainder) % 2;
-            remainder = (binary1 % 10 + binary2 % 10 + remainder) / 2;
-            binary1 = binary1 / 10;
-            binary2 = binary2 / 10;
-        }
-        if (remainder != 0)
-        {
-            sum[i++] = remainder;
-        }
-        --i;
-        while (i >= 0)
-        {
-            binary_prod_result = binary_prod_result * 10 + sum[i--];
-        }
-        return binary_prod_result;
+        // Create an HTTP server that listens on the specified port
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        // Create a context for a simple "Hello, World!" response
+        server.createContext("/", new HelloWorldHandler());
+
+        // Start the server
+        server.setExecutor(null); // Use the default executor
+        server.start();
+
+        System.out.println("Server is running on port " + port);
+    }
+}
+
+class HelloWorldHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        System.out.println("I recived a reguest");
+        String response = "Hello, World!";
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 }
