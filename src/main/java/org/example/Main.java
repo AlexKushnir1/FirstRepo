45a9aa2c1ed75111d7a41c9ecbf85f7025e30286
AlexKushnir1 <alex@alex-Precision-7510>
2023-10-19 12:59:22 +0300

    Replace basic java server for spark server

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,53 +1,69 @@
 package org.example;
 
-import com.sun.net.httpserver.HttpServer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.dto.GameStepDTO;
-import org.example.handlers.NewGameHandler;
-import org.example.handlers.PostDataHandler;
-import org.example.handlers.PostGameStepHandler;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
 
 public class Main {
     private static String[][] gameField = new String[3][3];
 
-    public static void main(String[] args) throws IOException {
-        int port = 8080; // Port to listen on
-
-        // Create an HTTP server that listens on the specified port
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-
-        // Context with response
-        server.createContext("/new_game", new NewGameHandler());
-        server.createContext("/post_data", new PostDataHandler());
-        server.createContext("/game-state", new PostGameStepHandler());
-
-        // Start the server
-        server.setExecutor(null); // Use the default executor
-        server.start();
-
-        System.out.println("Server is running on port " + port);
-
+    public static void main(String[] args) {
+        port(8080);
+        ObjectMapper objectMapper = new ObjectMapper();
+        post("/move", (request, response) -> {
+            try {
+                //gameStep -крок гри
+                GameStepDTO step = objectMapper.readValue(request.body(), GameStepDTO.class);
+                if (gameField[step.getX()][step.getY()] == null) {
+                    setStep(step);
+                    return "Received and processed the request.";
+                } else {
+                    return "Cell " + step.getX() + " : " + step.getY() + " is not empty";
+                }
+            } catch (JsonMappingException e) {
+                System.out.println(e);
+                response.status(400);
+                return "Bad request. Json can`t map data";
+            } catch (JsonProcessingException e) {
+                System.out.println(e);
+                response.status(400);
+                return "Bad request. Json can`t process";
+            }
+        });
+        post("/get", (request, response) -> {
+            try {
+                //Json that send to client
+                response.body(objectMapper.writeValueAsString(gameField));
+                return response.body();
+            } catch (JsonProcessingException e) {
+                response.status(400);
+                return "Bad request. Can`t write data to Json";
+            }
+        });
+        post("/new_game", (request, response) -> {
+            response.body("New game created/ for moving send Post request with coordinates");
+            gameField = new String[3][3];
+            return response.body();
+        });
     }
 
-    public static void setGameStep(GameStepDTO data) {
+    public static void setStep(GameStepDTO data) {
         int x = data.getX();
         int y = data.getY();
         String sign = data.getSign();
 
-        if (gameField[x][y] == null) {
-            gameField[x][y] = sign;
-        }
+        gameField[x][y] = sign;
+
         for (int i = 0; i <= 2; i++) {
             for (int j = 0; j <= 2; j++) {
                 System.out.print("   " + gameField[i][j]);
             }
             System.out.println(" ");
         }
-    }
-
-    public static String[][] getGameField() {
-        return gameField;
     }
 }
