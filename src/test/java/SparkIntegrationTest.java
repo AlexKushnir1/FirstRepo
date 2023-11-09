e5ed6b0cfada83ce27e8295a85a047e460b8b3d6
AlexKushnir1 <alex@alex-Precision-7510>
2023-11-09 18:20:40 +0200

    add unit test to SparkIntegrationTest

--- src/test/java/SparkIntegrationTest.java
+++ src/test/java/SparkIntegrationTest.java
@@ -1,11 +1,13 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.example.Main;
 import org.junit.Before;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SparkIntegrationTest {
     @Before
@@ -37,6 +39,11 @@ public class SparkIntegrationTest {
                 .extract()
                 .asString();
         assertThat(mustHaveErrorBecauseWrongCoordinates, is("Coordinate should be 0, 1, or 2"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testMovingFunction() {
+        assertEquals("0", 0);
     }
 
 }
